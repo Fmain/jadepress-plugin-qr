4c98c69d50a903298e2b1eca4bcaa46dcde5bceb
ZHAO Xudong <zxdong@gmail.com>
2016-05-13 22:39:35 +0800

    fix test

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -20,45 +20,45 @@ config.setting.plugins['jadepress-plugin-qr'] = '*'
 let init = require('jade-press').init
 
 co(function* () {
-  let dd = yield MongoClient.connect(config.setting.dbLink)
-  yield dd.dropDatabase()
-  let res = yield init(config)
-  return Promise.resolve(res)
+	let dd = yield MongoClient.connect(config.setting.dbLink)
+	yield dd.dropDatabase()
+	let res = yield init(config)
+	return Promise.resolve(res)
 })
 .then(function(app) {
 
-  app.listen(port, config.setting.listenAddress, function() {
-    console.log('' + new Date(), config.local.siteName, 'runs on port', port)
+	app.listen(port, config.setting.listenAddress, function() {
+		console.log('' + new Date(), config.local.siteName, 'runs on port', port)
 
 
-    // 2. run the nightwatch test suite against it
-    // to run in additional browsers:
-    //    1. add an entry in test/e2e/nightwatch.conf.json under "test_settings"
-    //    2. add it to the --env flag below
-    // For more information on Nightwatch's config file, see
-    // http://nightwatchjs.org/guide#settings-file
+		// 2. run the nightwatch test suite against it
+		// to run in additional browsers:
+		//    1. add an entry in test/e2e/nightwatch.conf.json under "test_settings"
+		//    2. add it to the --env flag below
+		// For more information on Nightwatch's config file, see
+		// http://nightwatchjs.org/guide#settings-file
 
-    // var spawn = require('cross-spawn')
-    // var runner = spawn(
-    //   './node_modules/.bin/nightwatch',
-    //   [
-    //     '--config', 'test/e2e/nightwatch.conf.js',
-    //     '--env', 'chrome'
-    //   ],
-    //   {
-    //     stdio: 'inherit'
-    //   }
-    // )
+		var spawn = require('cross-spawn')
+		var runner = spawn(
+			'./node_modules/.bin/nightwatch',
+			[
+				'--config', 'test/e2e/nightwatch.conf.js',
+				'--env', 'chrome'
+			],
+			{
+				stdio: 'inherit'
+			}
+		)
 
-    // runner.on('exit', function (code) {
-    //   process.exit(code)
-    // })
+		runner.on('exit', function (code) {
+			process.exit(code)
+		})
 
-    // runner.on('error', function (err) {
-    //   throw err
-    // })
+		runner.on('error', function (err) {
+			throw err
+		})
 
-  })
-  
+	})
+	
 })
 
